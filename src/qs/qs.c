70e88b5621760dec1bca341703780a23ca67e171
kevabouttown <kevabouttown@ubuntu.(none)>
2016-10-15 13:01:04 -0400

    qc.c main updated to handle no command line options or a filename

--- src/qs/qs.c
+++ src/qs/qs.c
@@ -6,37 +6,99 @@
 
 /*Read in inputs while there are inputs in file given in command line input */
 
+int takeInputs();
+int runFile();
+char *remove_newline(char *s);
+
 
 int main(int argc, char * argv[]){
 	
 	if(argc < 2){
-		printf("No filename included\n");
-		return -1;
+		takeInputs();
+		//printf("No filename included\n");
+		//return -1;
 	}
+	else{
+		char * f = argv[1];
+		if(runFile(f) == 0) return 0;
+		else return -1;
+	}
+
+	
+	return 0;
+}
+
+int takeInputs(){
+	char s[100];
+	char * p = s;
+	float a;
+	float b;
+	float c;
+
+
+	for(;;){
+
+		printf("Enter your floats for the quadratic equation: \n");
+		printf("A: ");
+		fgets(s, sizeof(s), stdin);
+		p = remove_newline(p);
+		a = atof(p);
+
+		printf("B: ");
+		fgets(s, sizeof(s), stdin);
+		p = remove_newline(p);
+		b = atof(p);
+
+		printf("C: ");
+		fgets(s, sizeof(s), stdin);
+		p = remove_newline(p);
+		c = atof(p);
+
+		printf("found %5.5f %5.5f %5.5f \n",a,b,c);
+		//call qsolv
+
+	}
+	return 0;
+}
+
+
+
+int runFile(char * fname){
+
 
 	FILE * fp;
 
-	if(!(fp = fopen(argv[1],"r"))) return -1;
+	if(!(fp = fopen(fname,"r"))) return -1;
 	
-	//char * readin = malloc(100);
 	char * token;
-	int a, b, c;
+	float a, b, c;
 	char readin[256];
 
 	while(fgets(readin,sizeof(readin),fp)){
 		token = strtok(readin," ");
-		a = atoi(token);
+		a = atof(token);
 		token = strtok(NULL," ");
-		b = atoi(token);
+		b = atof(token);
 		token = strtok(NULL," ");
-		c = atoi(token);
+		c = atof(token);
 
-		printf("found %d %d %d \n",a,b,c);
+		printf("found %5.5f %5.5f %5.5f \n",a,b,c);
 
 		//call qsolv
 	}
 
 	fclose(fp);
-	
+
 	return 0;
+
+}
+
+char *remove_newline(char *s)
+{
+    int len = strlen(s);
+
+    if (len > 0 && s[len-1] == '\n')  // if there's a newline
+        s[len-1] = '\0';          // truncate the string
+
+    return s;
 }
