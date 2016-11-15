3198a4c31dce42d13d9849088d4886e92a488d75
Joseph Owens <joseph.t.owens@wmich.edu>
2016-11-15 17:57:32 -0500

    modified qs.c to continue to loop even if caught errors are thrown

--- src/qs/qs.c
+++ src/qs/qs.c
@@ -1,6 +1,6 @@
 /* This program will read floats from command line inputs or read from a file
 in order to solve the quadratic equation. Inputs are taken in float form and 
-calculated using double precesion to IEEE standard.*/
+calculated using double precision to IEEE standard.*/
 #include "../quadsolver.h"
 
 
@@ -8,36 +8,68 @@ calculated using double precesion to IEEE standard.*/
 int 
 main(int argc, char * argv[])
 {
-	qInputs input;
+	int ret;
 
+	while(ret != 1){
+		ret = loop();
+	}
+}
+
+/*
+* Separate function to loop so we can reenter and continue to request inputs.
+*/
+int
+loop(){
+	qInputs input;
+	qsStruct q;
 	char readin[256];
+	int ret;
 
 	printf("Please enter a,b, and c separated by spaces\n (i.e., <a> <b> <c>) or \"q\" to quit\n");
 	//Inputs from command line
-	while(fgets(readin,sizeof(readin), stdin)) {
+	fgets(readin,sizeof(readin), stdin))
 
-		// Quit command
-		if(strA[0] == 'q' ) {
-			printf("Quitting...\n");
-			exit(0);
-		}
+	// Quit command
+	if(strA[0] == 'q' ) {
+		printf("Quitting...\n");
+		return 1;
+	}
 
-		input = takeInputs(readin);
-		if (input.errorCode != 0) {
-			// Raise error
-			// Output should handle error code
-			printf("Error, please try again");
-		}
-
-		// We're good
-		else {
-			printf("Running qusolve for a=%d, b=%d, c=%d", input.a, input.b, input.c);
-			//qsolve(input.a, input.b, input.c);
-
-			// End-- run loop again
-			printf("Please enter a,b, and c separated by spaces\n (i.e., <a> <b> <c>) or \"q\" to quit\n");
-		}
+	//Parse out inputs
+	input = takeInputs(readin);
+	if (input.errorCode != 0) {
+		// Raise error
+		// Output should handle error code
+		outputErrorVal(input->errorCode);
+		return 0; 
 	}
 	
-	return -2; //shouldn't reach this point
+	//No error? good
+	printf("Running qsolve for a=%d, b=%d, c=%d", input.a, input.b, input.c);
+	
+	q.a = input.a;
+	q.b = input.b;
+	q.c = input.c;
+
+	//run Qsolv
+	ret = qssolv(&q);
+	if(ret != 0){
+		// Raise error
+		// Output should handle error code
+		outputErrorVal(input->errorCode);
+		return 0; 
+	}
+
+	//format and output results
+	ret = formatting(&q);
+	if(ret != 0){
+		// Raise error
+		// Output should handle error code
+		outputErrorVal(input->errorCode);
+		return 0; 
+	}
+
+	//end
+
+	return 0;
 }
